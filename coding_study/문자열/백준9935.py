4a0753d4105178083ccf6898f7a64d91dec6ddd5
azi6511 <azi6511@naver.com>
2021-07-15 03:33:59 +0900

    Create 백준9935.py
    백준 9935

---
+++ coding_study/문자열/백준9935.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+s = list(input().rstrip())
+bomb = list(input().rstrip())
+
+stack = []
+
+for i in range(len(s)):
+    stack.append(s[i])
+    if len(stack)>=len(bomb) and bomb[-1]==stack[-1]:
+        if stack[-len(bomb):]==bomb:
+            for i in range(len(bomb)):
+                stack.pop()
+                
+if stack:
+    print("".join(stack))
+else:
+    print("FRULA")
